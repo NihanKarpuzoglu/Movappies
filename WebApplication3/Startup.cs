068da929085db8696ebbea1d2a209bc9fe5d9d4f
NihanKarpuzoglu <74454515+NihanKarpuzoglu@users.noreply.github.com>
2021-12-04 10:03:55 +0300

    Startup file updated

--- WebApplication3/Startup.cs
+++ WebApplication3/Startup.cs
@@ -55,7 +55,7 @@ namespace WebApplication3
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Movies}/{action=Index}/{id?}");
             });
         }
     }
